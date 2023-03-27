0f8a4a5d13ee5d48854362ed22c923423a5fca96
Kisa_shuSha <Kisa shusha@MacBook-Pro-Sergej.local>
2023-03-27 18:02:57 +0300

    change

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -31,7 +31,7 @@ export const scss = () => {
       cascade: true
     })))
     // Раскомментировать если нужен не сжатый дубль файла стилей
-    .pipe(app.gulp.dest(app.path.build.css))
+    // .pipe(app.gulp.dest(app.path.build.css))
     .pipe(app.plugins.if(app.isBuild, cleanCss()))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(app.gulp.dest(app.path.build.css))
